62d9399ea2e2c87a68ebb18e315262c3c9ace249
staslolpol <staspriho9@gmail.com>
2026-04-01 18:24:53 +0300

    Task03 in work

---
+++ Task03/logic.cpp
@@ -0,0 +1,18 @@
+#include "logic.h"
+
+bool recursion(int number) {
+	if (number == 1) {
+		return true;
+	}
+
+	if (number % 3 != 0) {
+		return false;
+	}
+
+	return recursion(number / 3);
+}
+
+
+int get_max_element(int array[], int size){
+	return 0;
+}
